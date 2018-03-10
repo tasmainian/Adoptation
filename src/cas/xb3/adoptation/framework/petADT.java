8ab0b4c6a360cf0cb2370be0786a7223ebc97138
Abdul <abdulelrahwan@gmail.com>
2018-03-10 02:08:34 -0500

    added ADT&reader files 💪🍆

---
+++ src/cas/xb3/adoptation/framework/petADT.java
@@ -0,0 +1,62 @@
+package cas.xb3.adoptation.framework;
+
+public class petADT {
+	
+	private final String ID;
+	private final String sex;
+	private final String yearsAge;
+	private final String monthsAge;
+	private final String primaryBreed;
+	private final String secondaryBreed;
+	private final String primaryColour;
+	private final String secondaryColour;
+	private final String species;
+	
+	public petADT(String id, String sex, String yearsAge, String monthsAge, String primaryBreed, String secondaryBreed, String primaryColour, String secondaryColour, String species) {
+		this.ID = id;
+		this.sex = sex;
+		this.yearsAge = yearsAge;
+		this.monthsAge = monthsAge;
+		this.primaryBreed = primaryBreed;
+		this.secondaryBreed = secondaryBreed;
+		this.primaryColour = primaryColour;
+		this.secondaryColour = secondaryColour;
+		this.species = species;
+	}
+	public String getID() {
+		return this.ID;
+	}
+	
+	public String getGender() {
+		return this.sex;
+	}
+	
+	public String getYearsAge() {
+		return this.yearsAge;
+	}
+	
+	public String getMonthsAge() {
+		return this.monthsAge;
+	}
+	
+	public String getPrimaryBreed() {
+		return this.primaryBreed;
+	}
+	
+	public String getSecondaryBreed() {
+		return this.secondaryBreed;
+	}
+	
+	public String getPrimaryColour() {
+		return this.primaryColour;
+	}
+	
+	public String getSecondaryColour() {
+		return this.secondaryColour;
+	}
+	
+	public String getSpecies() {
+		return this.species;
+	}
+
+}
