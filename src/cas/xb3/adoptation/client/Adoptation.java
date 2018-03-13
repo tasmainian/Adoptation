7434d871f4a1d5fc768a80141046a305f4501658
McFeelio <owenjmcneil@gmail.com>
2018-03-13 17:41:27 -0400

    Initial UI setup

--- src/cas/xb3/adoptation/client/Adoptation.java
+++ src/cas/xb3/adoptation/client/Adoptation.java
@@ -1,5 +1,11 @@
 package cas.xb3.adoptation.client;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * The <i>Adoptation</i> client's MainClass.
  * 
@@ -9,11 +15,19 @@ package cas.xb3.adoptation.client;
  * @author Owen McNeil, ...
  * @version 1.0
  */
-public class Adoptation {
+public class Adoptation extends Application {
 	
 	
 	public static void main(String args[]) {
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		primaryStage.setTitle("Adoptation");
+		primaryStage.setScene(new Scene(root, 800, 500));
+		primaryStage.show();
 	}
 
 }
---
+++ src/cas/xb3/adoptation/client/Controller.java
@@ -0,0 +1,24 @@
+package cas.xb3.adoptation.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+public class Controller implements Initializable {
+	
+	public ToggleGroup searchGroup;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("Loading data...");
+		
+		RadioButton selectedSearch = (RadioButton)searchGroup.getSelectedToggle();
+		System.out.println(selectedSearch.getText());
+	}
+	
+	
+
+}
