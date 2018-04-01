7f81decc27cbcde5c97ab354d3d9bb21834e1c18
Abdul <abdulelrahwan@gmail.com>
2018-04-01 15:29:22 -0400

    fix

--- src/cas/xb3/adoptation/framework/BinarySearch.java
+++ src/cas/xb3/adoptation/framework/BinarySearch.java
@@ -7,9 +7,10 @@ public class BinarySearch {
 	public static int binarySearch(String[] list, String key) {
 	    int low = 0;
 	    int high = list.length - 1;
+	    int mid;
 
 	    while (high >= low) {
-	      int mid = (low + high) / 2;
+	      mid = (low + high) / 2;
 	      
 	      if (list[mid].compareTo(key) < 0)
 	        low = mid + 1;
@@ -21,17 +22,61 @@ public class BinarySearch {
 
 	    return -1; 
 	  }
-/*
+	
+	 public static int lower_bound(String[] arr, String key) {
+	        int len = arr.length;
+	        int lo = 0;
+	        int hi = len-1;
+	        int mid = (lo + hi)/2;
+	        while (true) {
+	            int cmp = arr[mid].compareTo(key);
+	            if (cmp == 0 || cmp > 0) {
+	                hi = mid-1;
+	                if (hi < lo)
+	                    return mid;
+	            } else {
+	                lo = mid+1;
+	                if (hi < lo)
+	                    return mid<len-1?mid+1:-1;
+	            }
+	            mid = (lo + hi)/2;
+	        }
+	    }
+
+	    public static int upper_bound(String[] arr, String key) {
+	        int len = arr.length;
+	        int lo = 0;
+	        int hi = len-1;
+	        int mid = (lo + hi)/2;
+	        while (true) {
+	            int cmp = arr[mid].compareTo(key);
+	            if (cmp == 0 || cmp < 0) {
+	                lo = mid+1;
+	                if (hi < lo)
+	                    return mid<len-1?mid+1:-1;
+	            } else {
+	                hi = mid-1;
+	                if (hi < lo)
+	                    return mid;
+	            }
+	            mid = (lo + hi)/2;
+	        }
+	    }
+
 	public static void main(String[] args) {
 
-        String array[] ={"b","a","c","d","e","f","g"};
-        String search = "a";
+        String array[] ={"b","a","c","d","e","f","g", "c", "c" , "c", "c", "c"};
+        String search = "c";
 
         heapSort.sortHeap(array);
 
         int index = binarySearch(array,search);
+        int lowerBound = lower_bound(array, search);
+        int upperBound = upper_bound(array, search); 
 
-        System.out.println(index != -1 ? array[index]+ " - Index is "+index : "Not found");
-    }*/
-    
+        //System.out.println(index != -1 ? array[index]+ " - Index is "+index : "Not found");
+        System.out.println("reg binary: " + index + " lower: " + lowerBound + " upper: " + upperBound);
+        
+	}
+//    
 }
--- src/cas/xb3/adoptation/framework/MainQ.java
+++ src/cas/xb3/adoptation/framework/MainQ.java
@@ -45,33 +45,80 @@ public class MainQ {
      System.out.println(Arrays.toString(questions.getList()));
      
      ArrayList<petADT> pets = Parser.parse("data/Animal_Tag_DATA_2015_8.csv");
+     System.out.println(pets.size());
+     
      String[] petsBreeds = new String[pets.size()];
      for (int i = 0; i < pets.size(); i++){
     	 petsBreeds[i] = pets.get(i).getBreedGroup();
      }
      //System.out.println(Arrays.toString(petsBreeds));
      heapSort.sortHeap(petsBreeds);
+     
+     BufferedWriter outputWriter = null;
+     outputWriter = new BufferedWriter(new FileWriter("indices.txt"));
+     for (String s : petsBreeds) {
+    	 outputWriter.write(s);
+    	 outputWriter.newLine();
+    	 //System.out.println(s);
+     }
+     outputWriter.flush();  
+     outputWriter.close();
+     
      String[] petResults = questions.getList();
+
 	 ArrayList<Integer> petIndex = new ArrayList<Integer>();
 	 String[] temp = petsBreeds;
     // int[] petIndex;
      //int count = 0;
-	 System.out.println("bdaskjdhkjas");
-	 System.out.println(petsBreeds[103457]);
+//	 System.out.println("bdaskjdhkjas");
+//	 System.out.println(petsBreeds[103457]);
 
-     for (int i = 0; i < petResults.length; i++){
-    	 temp = petsBreeds;
-    	 while(BinarySearch.binarySearch(temp, petResults[i]) != -1){
-    		  petIndex.add(BinarySearch.binarySearch(temp, petResults[i]));
-    		  //petIndex[count] = BinarySearch.binarySearch(petsBreeds, petResults[i]);
-	       	  //count++;
-	       	  temp[BinarySearch.binarySearch(temp, petResults[i])] = "found";
-    	 }
-     }
-    for(int i =0; i< petIndex.size(); i++){
-    	System.out.println(petIndex.get(i));
-    }
+	 
+	 for (String s: petResults) {
+		 int lowBound = BinarySearch.lower_bound(temp, s);
+		 int highBound = BinarySearch.upper_bound(temp, s);
+		 
+		 for (int i = lowBound; i<highBound; i++) {
+			 petIndex.add(i);
+			 //System.out.println(x);
+		 }
+	 }
+	 
+	 
+	 
+	 
+	 
+//     for (int i = 0; i < petResults.length; i++){
+//    	 //temp = petsBreeds;
+//    	 while(BinarySearch.binarySearch(temp, petResults[i]) != -1){
+//    		 
+//			 int index = Arrays.binarySearch (temp, petResults[i]);
+//			 int first = index;
+//			 int last = index;
+//			 if (index >= 0) {
+//			     while (first > 0 && temp[first-1] == petResults[i])
+//			         first--;
+//			     while (last < temp.length - 1 && temp[last+1] == petResults[i])
+//			    	 last++;
+//			 }
+//    		 
+//    		  petIndex.add(BinarySearch.binarySearch(temp, petResults[i]));
+////    		  //petIndex[count] = BinarySearch.binarySearch(petsBreeds, petResults[i]);
+////	       	  //count++;
+//	       	  temp[BinarySearch.binarySearch(temp, petResults[i])] = "found";
+//    	 }
+//     }
+     
+     System.out.println(Arrays.toString(petIndex.toArray()));
+     
+//    for (int k : petIndex) {
+//    	System.out.println(petIndex.get(k));
+//    }
+    
+//    for(int i =0; i< petIndex.size(); i++){
+//    	System.out.println(petIndex.get(i));
+//    }
 
-    System.out.println(petsBreeds[103457]);
+//    System.out.println(petsBreeds[103457]);
     }
 }
