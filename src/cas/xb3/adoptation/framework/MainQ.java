6790a8d61ae2356b7a8c17fbe8b80a9fe2294b57
Tasmiha Hassan <htasmiha@gmail.com>
2018-04-02 19:33:26 -0400

    Cleaned it up a little

--- src/cas/xb3/adoptation/framework/MainQ.java
+++ src/cas/xb3/adoptation/framework/MainQ.java
@@ -41,11 +41,10 @@ public class MainQ {
 			System.out.println();
 		}
 		System.out.println();
-		System.out.println(Arrays.toString(questions.getList()));
+		//System.out.println(Arrays.toString(questions.getList()));
 
 		// ****************************ALGORITHMS IMPLEMENTATION***********************
 		ArrayList<petADT> pets = Parser.parse("data/Animal_Tag_DATA_2015_8.csv");
-		// System.out.println(pets.size());
 
 		// Loads all the pet breeds onto a string array
 		String[] petsBreeds = new String[pets.size()];
@@ -64,22 +63,12 @@ public class MainQ {
 		// Sorts the ADT in the same order as the pet breeds
 		PetHeapSort.sortHeap(pets);
 
-		// Testing the outputs to see if it works
-		/*
-		 * BufferedWriter outputWriter = null; outputWriter = new BufferedWriter(new
-		 * FileWriter("data/indices.txt")); for (String s : petsBreeds) {
-		 * outputWriter.write(s); outputWriter.newLine(); //System.out.println(s); }
-		 * outputWriter.flush(); outputWriter.close();
-		 */
-
 		// Obtains the list of results from the questionnaire
 		String[] petResults = questions.getList();
 
 		// idk
 		ArrayList<Integer> petIndex = new ArrayList<Integer>();
 		String[] temp = petsBreeds;
-		// int[] petIndex;
-		// int count = 0;
 
 		// Finds the first to last repetitive breed in the list
 		ArrayList<String> listPrimary = new ArrayList<String>();
@@ -98,45 +87,13 @@ public class MainQ {
 		ArrayList<petADT> primBreed = new ArrayList<petADT>();
 		for (int k = 0; k < petIndex.size(); k++) {
 			int ind = petIndex.get(k);
-			// System.out.println(ind);
 			primBreed.add(pets.get(ind));
 			petADT petOptions = pets.get(ind);
-			// System.out.println("Species: " + petOptions.getSpecies() + " Breed: " +
-			// petOptions.getPrimaryBreed() + " Age: " + petOptions.getYearsAge());
-
 		}
 
 		ArrayList<petADT> petsNew = pets;
-		PetHeapSortPrimary.sortHeap(petsNew);
-		
-		//System.out.println(Arrays.toString(petsNew.toArray()));
-		
-
-		String[] listPrimary2 = new String[listPrimary.size()];
-		for (int i =0; i<listPrimary2.length; i++) {
-			listPrimary2[i] = listPrimary.get(i); 
-		}
-		heapSort.sortHeap(listPrimary2);
-		ArrayList<Integer> petCount = new ArrayList<Integer>();
-		for (String s : listPrimary2) {
-			int lowBound = BinarySearch.lower_bound(petsPrimaryBreed, s);
-			int highBound = BinarySearch.upper_bound(petsPrimaryBreed, s);
-			// petCount.add(highBound-lowBound);
-			for (int i = lowBound; i < highBound; i++) {
-				petsNew.get(i).setCount(highBound - lowBound);
-				//saad.add(pets.get(i));
-			}
-		}
-		
-		for (petADT urmom : primBreed) {
-			//System.out.println(urmom.getBreedGroup());
-		}
-		
+		PetHeapSortPrimary.sortHeap(petsNew);		
 		PetHeapSortPrimary.sortHeap(primBreed);
-		
-		for (petADT s : primBreed) {
-			System.out.println(s.getPrimaryBreed());
-		}
 		
 		String[] finalBreeds = new String[primBreed.size()];
 		for (int i = 0; i<finalBreeds.length; i++) {
@@ -148,8 +105,6 @@ public class MainQ {
 		
 		Set<String> mySet = new HashSet<String>(asList);
 		for(String s: mySet){
-
-			//System.out.println(s + " " + Collections.frequency(asList,s));
 			finalPetADT finalpetshit = new finalPetADT(s,Collections.frequency(asList,s));
 			tas.add(finalpetshit);
 		}
@@ -159,9 +114,6 @@ public class MainQ {
 		for(int i = 0; i < 3; i++) {
 			System.out.println("Breed: " + tas.get(i).getPrimBreed() + " Count: " + tas.get(i).getCount());
 		}
-		//System.out.println(saad.get(0).getBreedGroup());
 		
-		
-		//System.out.println(petsNew.get(104000).getBreedGroup()+" " + petsNew.get(104000).getPrimaryBreed() +": Count =" + petsNew.get(104000).getCount());
 	}
 }
